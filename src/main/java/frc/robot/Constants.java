b3714d6dfe605a1783859944a1058317ee8573b8
Alex <sjuicep01@protonmail.com>
2025-10-28 20:20:07 -0600

    Made Constants
    pls put all constants like speed and stuff here MAKE SURE ANYTHING INSIDE HERE IS PUBLIC STATIC unless you know what your doing

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+public class Constants {
+    public static class driveConst{
+        public static double speedH = 0.6;
+        public static double speedL = 0.3;
+        public static double turnH = 0.3;
+        public static double turnL = 0.1;
+    }
+    public static class flywheelConst{
+
+    }
+    public static class intakeConst{
+
+    }
+}
