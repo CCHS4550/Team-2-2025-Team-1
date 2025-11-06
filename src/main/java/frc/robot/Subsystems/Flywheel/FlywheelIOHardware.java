f4d88a6c76b73840afece124fc395dec5926e63a
fortniteReference <3dplantersbusiness@gmail.com>
2025-11-06 16:47:03 -0700

    i made new function :)

--- src/main/java/frc/robot/Subsystems/Flywheel/FlywheelIOHardware.java
+++ src/main/java/frc/robot/Subsystems/Flywheel/FlywheelIOHardware.java
@@ -8,4 +8,13 @@ public class FlywheelIOHardware implements FlywheelIO{
     private final Spark flywheel1 = new Spark(4);
     private final Spark flywheel2 = new Spark(5);
 
+    private void spinMotor(double speed) {
+        flywheel1.set(speed);
+        flywheel2.set(speed);
+    }
+
+    private void stopMotor() {
+        flywheel1.stopMotor();
+        flywheel2.stopMotor();
+    }
 }
