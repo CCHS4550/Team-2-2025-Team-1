f38528af1cb382a1a1184651c888b16e3edd3764
Suhit <111322676+pancakecylinder@users.noreply.github.com>
2025-11-06 16:34:47 -0700

    old cod stinky

--- src/main/java/frc/robot/Subsystems/Flywheel/FlywheelIOHardware.java
+++ src/main/java/frc/robot/Subsystems/Flywheel/FlywheelIOHardware.java
@@ -4,24 +4,8 @@ import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import frc.robot.Constants;
 
 public class FlywheelIOHardware implements FlywheelIO{
-    private FlywheelIOInputs io;
 
     private final Spark flywheel1 = new Spark(4);
     private final Spark flywheel2 = new Spark(5);
 
-    public void launch(boolean active){
-        if(!active){
-            flywheel1.set(0);
-            flywheel2.set(0);
-            return;
-        }
-        if(io.speed == io.WheelSpeed.FAST){
-            flywheel1.set(Constants.flywheelConst.speedF);
-            flywheel2.set(Constants.flywheelConst.speedF);
-        }
-        if(io.speed == io.WheelSpeed.SLOW){
-            flywheel1.set(Constants.flywheelConst.speedS);
-            flywheel2.set(Constants.flywheelConst.speedS);
-        }
-    }
 }
