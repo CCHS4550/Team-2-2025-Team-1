1e2373d25d6c8ad4d33606e551a803c177267b88
fortniteReference <3dplantersbusiness@gmail.com>
2025-11-06 17:10:18 -0700

    Update Flywheel.java

--- src/main/java/frc/robot/Subsystems/Flywheel/Flywheel.java
+++ src/main/java/frc/robot/Subsystems/Flywheel/Flywheel.java
@@ -1,5 +1,11 @@
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Subsystems.Flywheel.FlywheelIO;
 
 public class Flywheel extends SubsystemBase{
-    
+    private FlywheelIO io;
+    public Flywheel(FlywheelIO flywheel) {
+        io = flywheel;
+    }
+    public void spinMotor(double speed) {}
+    public void stopMotor() {}
 }
