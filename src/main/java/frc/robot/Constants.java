9a1599478eba963fb855d3e13c5d59ba69fc9298
Alex <sjuicep01@protonmail.com>
2025-11-14 10:36:09 -0700

    SparkMax
    replaced all the sparks with maxes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,15 +16,15 @@ public class Constants {
     public static state robotState = state.DISABLE;
     public static class driveConst{
 
-        public static int LFID = 0;
-        public static int LBID = 1;
-        public static int RFID = 2;
-        public static int RBID = 3;
+        public static final int LFID = 0;
+        public static final int LBID = 1;
+        public static final int RFID = 2;
+        public static final int RBID = 3;
 
-        public static double speedH = 0.6;
-        public static double speedL = 0.3;
-        public static double turnH = 0.3;
-        public static double turnL = 0.1;
+        public static final double speedH = 0.6;
+        public static final double speedL = 0.3;
+        public static final double turnH = 0.3;
+        public static final double turnL = 0.1;
         
         public static SPEEDMODE driveSpeed = SPEEDMODE.HIGH;
     }
@@ -32,11 +32,11 @@ public class Constants {
         public static final int port2 = 0;
         public static final int port1 = 0;
 
-        public static double speed = 0.8; 
+        public static final double speed = 0.8; 
     }
     public static class IntakeConst{
-        public static int port = 6;
+        public static final int port = 6;
 
-        public static double speed = 0.5;
+        public static final double speed = 0.5;
     }
 }
