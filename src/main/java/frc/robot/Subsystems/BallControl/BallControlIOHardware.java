56070dbb3f470fbde88b3f42cadeffa99c0bc22a
Alex <sjuicep01@protonmail.com>
2025-11-21 12:43:58 -0700

    Reapply "SparkMax"
    This reverts commit 2fe653effdc6bb55edd7eb6c3c2c6473e789448e.

--- src/main/java/frc/robot/Subsystems/BallControl/BallControlIOHardware.java
+++ src/main/java/frc/robot/Subsystems/BallControl/BallControlIOHardware.java
@@ -1,10 +1,10 @@
 package frc.robot.Subsystems.BallControl;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import frc.robot.Constants.IntakeConst;
 
 
 public class BallControlIOHardware implements BallControlIO{
-    private final Spark ballControlMotor = new Spark(IntakeConst.port);
+    private final PWMSparkMax ballControlMotor = new PWMSparkMax(IntakeConst.port);
     public void forward (double speed){
         ballControlMotor.set(speed);
     }
